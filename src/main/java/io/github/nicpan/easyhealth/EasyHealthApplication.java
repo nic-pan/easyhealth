4e8c9d31c4d5ba8128cf8e66c28bd134547565ff
nicole <nicole@nicole-ThinkPad-T580>
2021-09-02 13:07:28 +0300

    initial commit

---
+++ src/main/java/io/github/nicpan/easyhealth/EasyHealthApplication.java
@@ -0,0 +1,13 @@
+package io.github.nicpan.easyhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyHealthApplication.class, args);
+	}
+
+}
